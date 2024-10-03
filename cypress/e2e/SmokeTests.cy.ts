0cb3f6ba000a01741e4b54621014c7c57278dff3
U769550 <nefo.tatari@lhind.dlh.de>
2024-10-03 20:41:04 +0200

    Add smoke tests

---
+++ cypress/e2e/SmokeTests.cy.ts
@@ -0,0 +1,7 @@
+describe("Smoke tests", () => {
+  it("ensures that the application is opened correctly", () => {
+    cy.visit("/");
+    cy.get(".card-header").should("exist").and("have.text", "Expeses Software");
+    cy.get(".btn").should("exist").and("contain", "Login");
+  });
+});
