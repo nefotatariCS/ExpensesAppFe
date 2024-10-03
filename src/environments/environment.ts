b3a87a57e7b30db1735cd70ad79a13a89442791b
RedianHoxha <51862189+RedianHoxha@users.noreply.github.com>
2024-10-03 15:10:37 +0200

    fix path

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: window.location.origin.includes('herokuapp.com')
-    ? 'https://expenses-app-be-5be9465e6f03.herokuapp.com/'
+    ? 'https://expenses-app-be-5be9465e6f03.herokuapp.com'
     : 'http://localhost:3000',
 };
