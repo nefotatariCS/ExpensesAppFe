6e46e5fc4f7f3125aee67f06da7a9d289f644b4e
RedianHoxha <51862189+RedianHoxha@users.noreply.github.com>
2024-10-03 11:22:56 +0200

    Change Server.js configuration

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-//Install express server
+// Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
+// Serve only the static files from the dist directory
 app.use(express.static('./dist/expenses-fe'));
 
-app.get('/*', (req, res) =
-    res.sendFile('index.html', {root: 'dist/expenses-fe/'}),
-);
+app.get('/*', (req, res) => {
+  res.sendFile('index.html', { root: 'dist/expenses-fe/' });
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
