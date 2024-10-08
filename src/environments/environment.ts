10e6ef6e70c3d5418a145d19594fe86242000777
U769550 <nefo.tatari@lhind.dlh.de>
2024-10-08 20:48:50 +0200

    Add the backend url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: window.location.origin.includes('herokuapp.com')
-    ? 'https://expenses-app-frontend-18c65f34b93b.herokuapp.com'
+    ? 'https://expenses-app-backend-d614d6eabde5.herokuapp.com'
     : 'http://localhost:3000',
 };
