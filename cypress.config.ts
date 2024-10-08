52110692db6f83743af337b89f1432c1b298c436
U769550 <nefo.tatari@lhind.dlh.de>
2024-10-08 22:17:10 +0200

    Do changes to e2e tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   e2e: {
     baseUrl: 'https://expenses-app-frontend-18c65f34b93b.herokuapp.com/',
     env: {
-      username: 'KGici',
+      username: 'TestUser',
       password: 'Test1234',
     },
     setupNodeEvents(on, config) {
--- cypress/e2e/Login.cy.ts
+++ cypress/e2e/Login.cy.ts
@@ -4,15 +4,7 @@ describe('template spec', () => {
     cy.visit('/');
     cy.login(Cypress.env('username'), Cypress.env('password'));
     cy.wait('@login').its('response.statusCode').should('eq', 200);
-
-    cy.logOut();
-  });
-  it('ensures that button color is orange', () => {
-    cy.visit('/');
-    cy.login(Cypress.env('username'), Cypress.env('password'));
-    cy.get('#loginButton')
-      .should('have.css', 'background-color')
-      .and('eq', '#2aa538'); //This should fail
+ 
     cy.logOut();
   });
 });
