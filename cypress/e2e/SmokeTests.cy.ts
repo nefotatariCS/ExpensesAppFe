4faa941ae5e298db870f10c4037b0be393b173d5
U769550 <nefo.tatari@lhind.dlh.de>
2024-10-09 23:09:02 +0200

    Add new changes

--- cypress/e2e/SmokeTests.cy.ts
+++ cypress/e2e/SmokeTests.cy.ts
@@ -1,7 +1,7 @@
 describe('Smoke tests', () => {
   it('ensures that the application is opened correctly', () => {
     cy.visit('/');
-    cy.get('.card-header').should('exist').and('have.text', 'Expeses App');
+    cy.get('.card-header').should('exist').and('have.text', 'Expenses App');
     cy.get('.btn').should('exist').and('contain', 'Log In');
   });
 });
